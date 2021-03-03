f3d662b9d4f4b779364ba537d64100ede9353f1c
Pinak Mapari <60837502+pinakmapari@users.noreply.github.com>
2021-03-03 21:02:29 +0530

    second commit

---
+++ android/app/src/main/kotlin/com/example/recipe_batao/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.recipe_batao
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:lit_firebase_auth/lit_firebase_auth.dart';
+
+import 'package:recipe_batao/screens/splash.dart';
+
+import 'config/palette.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return LitAuthInit(
+      authProviders: const AuthProviders(
+        emailAndPassword: true,
+        apple: true,
+        google: true,
+        twitter: true,
+      ),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Material App',
+        theme: ThemeData(
+            visualDensity: VisualDensity.adaptivePlatformDensity,
+            textTheme: GoogleFonts.muliTextTheme(),
+            accentColor: Palette.darkBlue,
+            appBarTheme: const AppBarTheme(
+              brightness: Brightness.light,
+              color: Palette.darkBlue,
+            )),
+        // home: LitAuthState(
+        //   authenticated: HomeScreen(),
+        //   unauthenticated: AuthScreen(),
+        // ),
+        home: SplashScreen(),
+      ),
+    );
+  }
+}
