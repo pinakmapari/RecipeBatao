a6e4528a76e27f8b6ffba36e0a490fb91a501cd9
Mrunmai Abhyankar <kshitiabhyankar@gmail.com>
2021-03-05 10:27:21 +0530

    profile section-1

---
+++ lib/screens/profile/profileEdit.dart
@@ -0,0 +1,10 @@
+class Profile {
+  final String name;
+  final String email;
+  final int phno;
+  final String image;
+  final int age;
+  final String cuisines;
+  Profile(
+      this.name, this.email, this.phno, this.image, this.age, this.cuisines);
+}
